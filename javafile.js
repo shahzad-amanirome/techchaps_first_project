c0a9caf7ffce6258887547aac8b9f0f71e89ce77
login <email2shahzadahmad@gmail.com>
2023-11-23 13:19:10 +0500

    add all my new project

---
+++ javafile.js
@@ -0,0 +1,12 @@
+<html>
+    <body>
+        <h2>
+            My first Javascript file
+        </h2>
+        <button type = "button" onClick = "document.getElementById('demo').innerHTML = Date()">
+            Showing Date
+            </button> 
+
+            <p id = "demo"></p>
+    </body>
+</html>
